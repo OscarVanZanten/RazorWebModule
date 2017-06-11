55196bae6a7ff565f0552f7b64d1dbd895b10bfb
InstaPizza <ovanzanten@gmail.com>
2017-06-11 07:22:59 +0200

    Module Registry added

--- Module.cs
+++ Module.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 
+using RazorWebModule.Util;
+
 namespace RazorWebModule
 {
     /// <summary>
@@ -9,6 +11,12 @@ namespace RazorWebModule
     /// </summary>
     public class Module
     {
+        /// <summary>
+        /// Registry that keeps count of the modules
+        /// </summary>
+        public static Registry<Module> Registry { get { if (registry == null) { registry = new Registry<Module>(); } return registry; } set { registry = value; } }
+        private static Registry<Module> registry;
+
         /// <summary>
         /// Module name
         /// </summary>
---
+++ Util/Registry.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorWebModule.Util
+{
+    /// <summary>
+    /// registry for holding data
+    /// </summary>
+    /// <typeparam name="T">type of said data</typeparam>
+    public class Registry<T>
+    {
+        /// <summary>
+        /// Items held
+        /// </summary>
+        Dictionary<string, T> items;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Registry()
+        {
+            items = new Dictionary<string, T>();
+        }
+
+        /// <summary>
+        /// Add entry
+        /// </summary>
+        /// <param name="name">name of entry</param>
+        /// <param name="value">value of entry</param>
+        public void Add(string name, T value)
+        {
+            items.Add(name, value);
+        }
+
+        /// <summary>
+        /// Try to get value by name
+        /// </summary>
+        /// <param name="name">name of entry being searched</param>
+        /// <returns>value</returns>
+        public T Get(string name)
+        {
+            T result = default(T);
+            items.TryGetValue(name, out result);
+            return result;
+        }
+
+    }
+}
